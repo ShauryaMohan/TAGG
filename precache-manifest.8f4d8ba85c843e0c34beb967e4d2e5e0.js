fa22c2a16e9ffce36438c0349267fd3147ab7ae1
Shaurya Mohan <shaurya.mhn@gmail.com>
2020-09-20 00:26:14 +0530

    Updates

---
+++ precache-manifest.8f4d8ba85c843e0c34beb967e4d2e5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8638a97754ec40a3d49b13930eff23f3",
+    "url": "/TAGG/index.html"
+  },
+  {
+    "revision": "57874ac62b4f9f0dbbbf",
+    "url": "/TAGG/static/css/main.c20919f1.chunk.css"
+  },
+  {
+    "revision": "a4315190796d958bfded",
+    "url": "/TAGG/static/js/2.580d4f6b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/TAGG/static/js/2.580d4f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57874ac62b4f9f0dbbbf",
+    "url": "/TAGG/static/js/main.a92fb31d.chunk.js"
+  },
+  {
+    "revision": "60020908fc371e8923db",
+    "url": "/TAGG/static/js/runtime-main.f2420960.js"
+  }
+]);
